448a82477311160df6a39705d065828856a3448d
Sofiane5900 <iguedjetalsofiane@gmail.com>
2024-01-21 00:41:34 +0100

    Ajout engine 2D Matter.js

--- main.js
+++ main.js
@@ -1,24 +1,59 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import {Engine, Render, Runner, Bodies, World } from "matter-js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const engine = Engine.create();
+const render = Render.create({
+  engine, 
+  element: document.body,
+  options: {
+    wireframes: false,
+    background: "#e4d9b3",
+    width: 620,
+    height: 850,  
+  },
+});
 
-setupCounter(document.querySelector('#counter'))
+
+const world = engine.world;
+
+const ground = Bodies.rectangle(310, 820, 620, 60, {
+  isStatic: true,
+  render: { 
+    fillStyle: "#bfa97a", 
+  }
+});
+
+
+const leftWall = Bodies.rectangle(15, 395, 30, 790, {
+  isStatic: true,
+  render: { 
+    fillStyle: "#bfa97a", 
+  }
+});
+
+
+const rightWall = Bodies.rectangle(605, 395, 30, 790, {
+  isStatic: true,
+  render: { 
+    fillStyle: "#bfa97a", 
+  }
+});
+
+
+
+World.add(world, [ground, leftWall, rightWall]);
+
+Render.run(render);
+Runner.run(engine);
+
+function addCurrentFruit() {
+  const body = Bodies.circle(300, 50, 100, {
+    render: {
+      fillStyle: "green",
+    },
+
+  });
+
+  World.add(world, body);
+}
+
+addCurrentFruit();
