3996a22aeb908a15af588fa1d360463cfbdc60ac
Sofiane5900 <iguedjetalsofiane@gmail.com>
2024-01-21 20:34:38 +0100

    ajout déplacement horizontal et géneration de fruit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {Engine, Render, Runner, Bodies, World } from "matter-js";
+import {Engine, Render, Runner, Bodies, World, Body, Sleeping } from "matter-js";
 
 const engine = Engine.create();
 const render = Render.create({
@@ -45,15 +45,68 @@ World.add(world, [ground, leftWall, rightWall]);
 Render.run(render);
 Runner.run(engine);
 
-function addCurrentFruit() {
-  const body = Bodies.circle(300, 50, 100, {
-    render: {
-      fillStyle: "green",
-    },
+let currentFruit = null;
+let interval = null;
+let disableAction = false; 
 
+function addCurrentFruit() {
+  const body = Bodies.circle(300, 50, 20, {
+    isSleeping: true,
+    render: {
+      fillStyle: "black",
+    },
   });
 
+
+  currentFruit = body;
   World.add(world, body);
+}
+
+window.onkeydown = (event) => {
+  console.log(event);
+
+  switch (event.code) {
+    case "ArrowLeft":
+      if (interval) return;
+      interval = setInterval(() => {
+        if (currentFruit.position.x - 20 > 30)
+          Body.setPosition(currentFruit, {
+            x: currentFruit.position.x - 1,
+            y: currentFruit.position.y,
+          });
+      }, 5);
+      break; // Add break statement here
+
+    case "ArrowRight":
+      if (interval) return;
+      interval = setInterval(() => {
+        if (currentFruit.position.x + 20 < 590)
+          Body.setPosition(currentFruit, {
+            x: currentFruit.position.x + 1,
+            y: currentFruit.position.y,
+          });
+      }, 5);
+      break; // Add break statement here
+
+    case "Space":
+      if (disableAction) return;
+      disableAction = true;
+      Sleeping.set(currentFruit, false);
+      setTimeout(() => {
+        addCurrentFruit();
+        disableAction = false;
+      }, 1000);
+  }
+};
+
+window.onkeyup = (event) => { 
+  switch (event.code) {
+    case "ArrowRight":
+    case "ArrowLeft":
+      clearInterval(interval);
+      interval = null;
+  }
+
 }
 
 addCurrentFruit();
